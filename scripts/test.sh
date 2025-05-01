3332bedccf6c8eb4f88c72ca415e3560e2effb18
Mostafa Kamal <hiremostafa@gmail.com>
2025-05-02 05:45:54 +0600

    docs: add doc for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,7 +10,7 @@ pytest \
     --cov-report=term-missing \
     --cov-report=html \
     --cov-report=xml \
-    --cov-fail-under=70 \
+    --cov-fail-under=80 \
     --cov-config=.coveragerc \
     -v \
     app/tests/ \
