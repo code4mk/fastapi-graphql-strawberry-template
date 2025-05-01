af39624c21d1c69442dac39dba114ff296ead0b9
Mostafa Kamal <hiremostafa@gmail.com>
2025-05-02 02:34:42 +0600

    refactor: add authorization able fields

--- app/config/authorization.py
+++ app/config/authorization.py
@@ -1,2 +1,10 @@
 # List of GraphQL fields that require authorization checks before execution.
-AUTHORIZATION_ABLE_FIELDS = ["users"]
+AUTHORIZATION_ABLE_FIELDS = [
+    # Query fields
+    "users",
+    "s_raw_sql_users",
+
+    # Mutation fields
+    "user_update",
+    "user_delete"
+]
